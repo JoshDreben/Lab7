8310b89c085208118d2bd798f481a104dad6bda3
Josh Dreben <joshdreben@gmail.com>
2021-05-13 23:30:49 -0400

    Update router.js

--- scripts/router.js
+++ scripts/router.js
@@ -10,17 +10,17 @@ const title = document.querySelector("header").querySelector("h1");
  */
 router.setState = function (state) {
   if (state === "home") {
-    history.pushState({ page: "home" }, "", "/");
+    history.pushState({ page: "home" }, "", "/Lab7");
     body.className = "";
     title.innerHTML = "Journal Entries";
   } else if (state === "settings") {
-    history.pushState({ page: "settings" }, "", "/#settings");
+    history.pushState({ page: "settings" }, "", "/Lab7#settings");
     title.innerHTML = "Settings";
     body.className = "settings";
   } else if (typeof state === "object") {
     body.removeChild(document.querySelector("entry-page"));
     body.appendChild(document.createElement("entry-page"));
-    history.pushState({ page: `Entry ${state.num}` }, "", `#entry${state.num}`);
+    history.pushState({ page: `Entry ${state.num}` }, "", `/Lab7#entry${state.num}`);
     body.className = "single-entry";
     document.querySelector("entry-page").entry = state.data;
     title.innerHTML = `Entry ${state.num}`;
